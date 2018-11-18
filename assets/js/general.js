b6e26a60545b6fb3a454c73ff4f64d4f168e4810
cd3770rn <35351418+cd3770rn@users.noreply.github.com>
2018-11-18 09:39:46 -0600

    Update general.js

--- assets/js/general.js
+++ assets/js/general.js
@@ -1,4 +1,6 @@
-
+import Vue from 'vue'
+import { MdButton, MdContent, MdTabs } from 'vue-material/dist/components'
+import 'vue-material/dist/vue-material.min.css'
 
 function startApp() {
   Vue.use(VueMaterial.default)
